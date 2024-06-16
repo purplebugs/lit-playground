16e36277de3a4725de4c0488dbad988358207660
Anita Lipsky <anita@purplebugs.com>
2024-06-16 17:20:05 +0200

    feat: Respond to click event using lit

--- element.js
+++ element.js
@@ -4,15 +4,21 @@ console.log("hello world from element.js");
 export class AlpacaElement extends LitElement {
   static properties = {
     name: { type: String },
+    counter: { type: Number },
   };
 
   constructor() {
     super();
     this.name = "Chanel";
+    this.counter = 0;
+  }
+
+  _increase() {
+    this.counter++;
   }
 
   render() {
-    return html`<h3>Alpaca: ${this.name}</h3>`;
+    return html`<h3 @click="${this._increase}">Alpaca: ${this.name} - Counter: ${this.counter}</h3>`;
   }
 }
 
