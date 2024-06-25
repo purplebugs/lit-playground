3ca33729c59342d020e69147b6259b36269d383f
Anita Lipsky <7695311+purplebugs@users.noreply.github.com>
2024-06-26 01:15:12 +1000

    build: Custom build so can support Google maps marker clustering package in upcoming feature (#13)

---
+++ build.js
@@ -0,0 +1,38 @@
+import rollupPluginCommonjs from "@rollup/plugin-commonjs";
+import rollupPluginResolve from "@rollup/plugin-node-resolve";
+import rollupPluginReplace from "@rollup/plugin-replace";
+import rollupPluginTerser from "@rollup/plugin-terser";
+import { rollup } from "rollup";
+
+const inputOptions = {
+  input: ["./src/alpaca-map.js"],
+  plugins: [
+    rollupPluginResolve({ preferBuiltins: true }),
+    rollupPluginCommonjs({ include: /node_modules/ }),
+    rollupPluginReplace({
+      "process.env.NODE_ENV": JSON.stringify("production"),
+      "preventAssignment": true,
+    }),
+    rollupPluginTerser({ format: { comments: false } }),
+  ],
+};
+
+const outputOptionsList = [
+  {
+    // preserveModules: true,
+    // dir: './public/build',
+    file: "./public/build/alpaca-map.js",
+    format: "es",
+    sourcemap: true,
+  },
+];
+
+let bundle = await rollup(inputOptions);
+
+for (const outputOptions of outputOptionsList) {
+  await bundle.write(outputOptions);
+}
+
+if (bundle) {
+  await bundle.close();
+}
--- web-dev-server.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  open: false,
-  watch: true,
-  appIndex: "index.html",
-  nodeResolve: {
-    exportConditions: ["development"],
-  },
-};
