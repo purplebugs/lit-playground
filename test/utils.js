5523a7fb9062ebc9ffc29908fb201401ce4971f3
Anita Lipsky <anita@purplebugs.com>
2024-07-04 06:50:15 +0200

    test: compareExact() false, compareSparse() true and false

--- test/utils.js
+++ test/utils.js
@@ -2,11 +2,58 @@ import { compareExact, compareSparse } from "../src/utils.js";
 import assert from "node:assert/strict";
 import { test } from "node:test";
 
-test(`compareExact() of equal objects`, () => {
-  const template = { shop: true, bookable: false };
-  const obj = { shop: true, bookable: false };
+test(`compareExact() of equal objects - return true`, () => {
+  const template = {
+    private: false,
+    public: true,
+  };
+  const obj = { private: false, public: true };
 
-  assert.ok(compareExact(template, obj));
+  assert.equal(compareExact(template, obj), true);
 });
 
-// TODO build out tests
+test(`compareExact() of non equal objects - return false`, () => {
+  const template = {
+    private: false,
+    public: true,
+  };
+  const obj = { private: true, public: true };
+
+  assert.equal(compareExact(template, obj), false);
+});
+
+test(`compareSparse() contains equal key, values - return true`, () => {
+  const template = {
+    bookable: true,
+    shop: true,
+  };
+
+  const obj = {
+    alpacaSales: true,
+    alpacaWalking: true,
+    bookable: true,
+    shop: true,
+    overnightStay: true,
+    studServices: false,
+  };
+
+  assert.equal(compareSparse(template, obj), true);
+});
+
+test(`compareSparse() contains non-equal key, values - return false`, () => {
+  const template = {
+    bookable: true,
+    shop: true,
+  };
+
+  const obj = {
+    alpacaSales: true,
+    alpacaWalking: true,
+    bookable: false,
+    shop: true,
+    overnightStay: true,
+    studServices: false,
+  };
+
+  assert.equal(compareSparse(template, obj), false);
+});
