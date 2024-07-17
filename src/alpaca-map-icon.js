8a79ce4418c76a77e9e8d8ea1075f5c7f748c59a
Anita Lipsky <anita@purplebugs.com>
2024-07-17 19:42:24 +0200

    feat: Icon house flag as web component

--- src/alpaca-map-icon.js
+++ src/alpaca-map-icon.js
@@ -35,6 +35,12 @@ export default class AlpacaMapIcon extends LitElement {
     super.connectedCallback();
   }
 
+  _iconHouseFlag() {
+    const path = svg`<path d="M480 0c-17.7 0-32 14.3-32 32V192 512h64V192H624c8.8 0 16-7.2 16-16V48c0-8.8-7.2-16-16-16H512c0-17.7-14.3-32-32-32zM416 159L276.8 39.7c-12-10.3-29.7-10.3-41.7 0l-224 192C1 240.4-2.7 254.5 2 267.1S18.6 288 32 288H64V480c0 17.7 14.3 32 32 32h64c17.7 0 32-14.3 32-32V384c0-17.7 14.3-32 32-32h64c17.7 0 32 14.3 32 32v96c0 17.7 14.3 32 32 32h64.7l.2 0h-1V159z" />`;
+
+    return html`<svg class="icon public" viewBox="0 0 640 512">${path}</svg>`;
+  }
+
   _iconKey() {
     const path = svg`<path d="M336 352c97.2 0 176-78.8 176-176S433.2 0 336 0S160 78.8 160 176c0 18.7 2.9 36.8 8.3 53.7L7 391c-4.5 4.5-7 10.6-7 17v80c0 13.3 10.7 24 24 24h80c13.3 0 24-10.7 24-24V448h40c13.3 0 24-10.7 24-24V384h40c6.4 0 12.5-2.5 17-7l33.3-33.3c16.9 5.4 35 8.3 53.7 8.3zM376 96a40 40 0 1 1 0 80 40 40 0 1 1 0-80z"/>`;
 
@@ -50,6 +56,9 @@ export default class AlpacaMapIcon extends LitElement {
   render() {
     let icon;
     switch (this.icon) {
+      case "houseFlag":
+        icon = this._iconHouseFlag();
+        break;
       case "key":
         icon = this._iconKey();
         break;
